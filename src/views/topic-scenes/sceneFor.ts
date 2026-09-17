e3dee904c155dac87a170d4d998f1e7f4d1aa0af
anajumorandii-eng <anaju.morandii@gmail.com>
2026-09-16 22:57:30 -0300

    Resolve conflito com Física em sceneFor

--- src/views/topic-scenes/sceneFor.ts
+++ src/views/topic-scenes/sceneFor.ts
@@ -6,11 +6,12 @@ import { geografia } from './data/geografia';
 import { literatura } from './data/literatura';
 import { matematica } from './data/matematica';
 import { quimica } from './data/quimica';
+import { fisica } from './data/fisica';
 import { biologia } from './data/biologia';
 import { validarLastro } from './lastro';
 import type { SceneEntry } from './types';
 
-const todas: SceneEntry[] = [...filosofia, ...sociologia, ...historia, ...geografia, ...literatura, ...matematica, ...quimica, ...biologia];
+const todas: SceneEntry[] = [...filosofia, ...sociologia, ...historia, ...geografia, ...literatura, ...matematica, ...quimica, ...fisica, ...biologia];
 const capitulos = new Map(interactiveSummaries.map((summary) => [summary.id, summary]));
 const validas = todas.filter((entry) => validarLastro(entry, capitulos.get(entry.chapterId)).length === 0);
 const porCapitulo = new Map(validas.map((entry) => [entry.chapterId, entry]));
