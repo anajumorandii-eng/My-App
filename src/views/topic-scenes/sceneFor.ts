1e1409cf1bd80a42a181d863a8559edc89dd8db9
anajumorandii-eng <anaju.morandii@gmail.com>
2026-09-16 22:58:48 -0300

    Alinha sceneFor com main antes do merge

--- src/views/topic-scenes/sceneFor.ts
+++ src/views/topic-scenes/sceneFor.ts
@@ -7,11 +7,10 @@ import { literatura } from './data/literatura';
 import { matematica } from './data/matematica';
 import { quimica } from './data/quimica';
 import { fisica } from './data/fisica';
-import { biologia } from './data/biologia';
 import { validarLastro } from './lastro';
 import type { SceneEntry } from './types';
 
-const todas: SceneEntry[] = [...filosofia, ...sociologia, ...historia, ...geografia, ...literatura, ...matematica, ...quimica, ...fisica, ...biologia];
+const todas: SceneEntry[] = [...filosofia, ...sociologia, ...historia, ...geografia, ...literatura, ...matematica, ...quimica, ...fisica];
 const capitulos = new Map(interactiveSummaries.map((summary) => [summary.id, summary]));
 const validas = todas.filter((entry) => validarLastro(entry, capitulos.get(entry.chapterId)).length === 0);
 const porCapitulo = new Map(validas.map((entry) => [entry.chapterId, entry]));
